5c980cf5447126f7ad8e556ba2f663a9b4589bb3
Sean Siganoff <nationwideclassics@yahoo.com>
2025-06-14 15:17:53 -0400

    updated the quick quote

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,32 +3,32 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <>
-        <div className="footer-container">
-                <div className="footer-overlay" id="contact-us">
-                    <div className="contact-us">
-                        <h3>CONTACT</h3>
-                        <div className="contact-container">
-                            <div className="desktop-footer">
-                                <ul> Call or text for a free estimate.
-                                    <li>Text: <b><a href="sms:469-969-5119">469-969-5119</a></b></li>
-                                    <li>Call: <b><a href="tel:469-969-5119">469-969-5119</a></b></li>
-                                    
-                            
-                                </ul>
-                            </div>
+    <footer>
+        <div className="footer">
+            <div className="footer-overlay" id="contact-us">
+                <div className="contact-us">
+                    <h3 className='theme-color'>CONTACT US</h3>
+                    <div className="contact-container">
+                        <div className="mobile-footer">
+                            <a href="sms:469-969-5119"><img className="my-icons" src={require("../../images/text-icon.png")} alt='SMS message icon'/></a>
+                            <a href="tel:469-969-5119"><img className="my-icons" src={require("../../images/phone-icon.png")} alt='Phone icon'/></a>
+                            {/* <a href="mailto:mobiledentandscratchrepair@yahoo.com"><img className="my-icons" src={require("../../images/email-icon.webp")} alt='Email icon'/></a> */}
+                        </div>
+                        <div className="desktop-footer">
+                        <p>CALL OR TEXT:  <b>469-969-5119</b></p>
                         </div>
                     </div>
-                    <div className="contact-us">
-                        <h3>HOURS</h3>
-                        <p>Monday - Saturday: 9AM - 9PM. <br />
-                        Sunday: 12PM - 4PM</p>
-                    </div>
                 </div>
+                <div className="contact-us">
+                    <h3 className='theme-color'>HOURS</h3>
+                    <p>Monday - Saturday: 9AM - 8PM.</p>
+                </div>
+            </div>
         </div>
-            
-        
-    </>
+        <div className="copyright">
+            BROTHERS MOBILE DENT AND SCRATCH REPAIR LLC 2025.
+        </div>
+    </footer>
   )
 }
 
--- src/components/QuickQuote/QuickQuote.js
+++ src/components/QuickQuote/QuickQuote.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './QuickQuote.css';
 
+import './QuickQuote.css';
+import { Link } from 'react-router-dom';
 const QuickQuote = () => {
   return (
     <div>
@@ -11,12 +11,15 @@ const QuickQuote = () => {
               <br />
               <br />
               <p>
-             <b> STEP 1. </b>  Send a few pictures via text, of the damage you need repaired to <a href="sms:469-969-5119">469-969-5119.</a> Also, please include the year make and model of the vehicle.
+             <b> STEP 1. </b>  Send a few pictures of the damage you need repaired via text messaging, to <a href="sms:469-969-5119">469-969-5119.</a> Please include the year make and model of the vehicle.
               <br /><br />
               <b> STEP 2. </b>  Wait for us to send you a quote.
 
               <br /><br />
-              <b> STEP 3. </b>  If you are satisfied with the quote and want to proceed with the repairs, let us know and we will setup a date and time that works for you.
+              <b> STEP 3. </b>  If you are satisfied with the quote and want to proceed, let us know and we will setup a date and time that works for you to come out and do the repairs.
+              <br />
+              <br />
+               <h4>All our work comes with a 100% satisfaction guarantee and a Lifetime Warranty.</h4><Link to={'/lifetime-warranty'}><button className="guaranteeBtn">CLICK HERE TO LEARN MORE!</button></Link>
             </p>
         </div>
     </div>
