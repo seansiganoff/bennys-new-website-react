b61497340a9210b5e1944ab3e35690c518e6b3ef
Sean Siganoff <nationwideclassics@yahoo.com>
2026-03-15 16:03:52 -0400

    removed car logos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import ReviewsSection from '../ReviewsSection/ReviewsSection';
 const Home = () => {
   return (
     <div className='home-container'>
-      <div className="vehicle-icons">
+      {/* <div className="vehicle-icons">
         <div className="vehicle-icons-inner-div-top" >
                 <img src={require('../../images/tesla.png')} alt='Tesla logo'/>
                 <img src={require('../../images/mercedes-icon.png')} alt='Mercedes logo'/>
@@ -15,9 +15,9 @@ const Home = () => {
                 <img src={require('../../images/porsche-icon.png')} alt='Porsche logo'/>
                 <img src={require('../../images/bmw-icon.png')} alt='BMW logo'/>
                 <img src={require('../../images/caddilic-icon.jpg')} alt='Cadillac logo'/>
-            </div>
         </div>
-        <div className='logo-div'>
+      </div> */}
+      <div className='logo-div'>
             <img className="company-logo" src={require("../../images/new-company-logo1.jpg")} alt='Company Logo'/>
             <p>
                 
@@ -27,7 +27,7 @@ const Home = () => {
             <br />
         <div className="locations"><b className='theme-color'>SERVING:</b> Dallas, Plano, Richardson, Frisco, Mckinney, Prosper, Coppel, Irving, Arlington, Lewisville, Flower Mound, Grapevine, Southlake.</div>
 
-            <div className="vehicle-icons">
+            {/* <div className="vehicle-icons">
               <div className="vehicle-icons-inner-div-bottom" >
                   <img src={require('../../images/toyota-icon.jpg')} alt='Toyota logo'/>
                   <img src={require('../../images/lexus-icon.png')} alt='Lexus logo'/>
@@ -35,9 +35,8 @@ const Home = () => {
                   <img src={require('../../images/chevy-icon.jpg')} alt='Infiniti logo'/>
                   <img src={require('../../images/honda-icon.png')} alt='Honda logo'/>
                   <img src={require('../../images/ford-icon.png')} alt='Lamborghini logo'/>
-              </div>
-            
-        </div>
+            </div>
+          </div> */}
         <br />
         
 
